78cbb4f1d7c66bbf1055dd8d9ccaf4ff4b821ae1
Joel Kronborg <joelkronborg@hotmail.com>
2016-04-15 15:15:36 +0100

    Cleaned up Agulhas test case and added particle spawning along the coast

--- tests/test_agulhas.py
+++ tests/test_agulhas.py
@@ -1,44 +1,32 @@
 from parcels import Grid, Particle, JITParticle, AdvectionRK4, AdvectionEE
 from argparse import ArgumentParser
 import numpy as np
-import math  # NOQA
 
 
 method = {'RK4': AdvectionRK4, 'EE': AdvectionEE}
 
 
-def agulhas_grid(xdim=20, ydim=20):
-    # Set grid variables
-    depth = np.zeros(1, dtype=np.float32)
-    time = np.zeros(1, dtype=np.float64)
-
-    lon = np.linspace(0, 100, xdim)
-    lat = np.linspace(0, 100, ydim)
-    U = np.ones((xdim, ydim), dtype=np.float32)
-    V = -np.ones((xdim, ydim), dtype=np.float32)
-    U[:, :xdim/2] = np.nan
-    V[:, :xdim/2] = np.nan
-
-    return Grid.from_data(U, lon, lat, V, lon, lat,
-                          depth, time)
-
-
-def agulhas_example(grid, npart, mode='jit', degree=1,
+def agulhas_example(grid, mode='jit', degree=1,
                    verbose=False, output=True, method=AdvectionRK4):
     # Determine particle class according to mode
     ParticleClass = JITParticle if mode == 'jit' else Particle
 
+    # Get coastal points by subtracting the masks of u and v
+    u0 = np.isnan(grid.U.data[0,:,:])
+    v0 = np.isnan(grid.V.data[0,:,:])
+    border = u0 - v0
+    lon, lat = grid.V.lon[np.where(border)[1]], grid.U.lat[np.where(border)[0]]
+
     # Initialise particles
-    x = (31, 32)
-    y = (-32, -32)
-    pset = grid.ParticleSet(npart, pclass=ParticleClass, start=(x[0], y[0]), finish=(x[1], y[1]))
+    npart = len(lon)
+    pset = grid.ParticleSet(npart, pclass=ParticleClass, lon=lon, lat=lat)
 
     if verbose:
         print("Initial particle positions:\n%s" % pset)
 
-    # Advect the particles for 24h
+    # Advect the particles for 100 days
     time = 24 * 360000.
-    dt = 36000.
+    dt = 12000.
     substeps = 1
     out = pset.ParticleFile(name="AgulhasParticle") if output else None
     print("Agulhas: Advecting %d particles for %d timesteps"
@@ -55,13 +43,10 @@ def agulhas_example(grid, npart, mode='jit', degree=1,
 
 if __name__ == "__main__":
     p = ArgumentParser(description="""
-Example of particle advection around an idealised peninsula""")
+Example of advection of particles released off the coast of South Africa and
+surroundings""")
     p.add_argument('mode', choices=('scipy', 'jit'), nargs='?', default='jit',
-                   help='Execution mode for performing RK4 computation')
-    p.add_argument('-p', '--particles', type=int, default=20,
-                   help='Number of particles to advect')
-    p.add_argument('-d', '--degree', type=int, default=1,
-                   help='Degree of spatial interpolation')
+                   help='Execution mode for performing computation')
     p.add_argument('-v', '--verbose', action='store_true', default=False,
                    help='Print particle information before and after execution')
     p.add_argument('-o', '--nooutput', action='store_true', default=False,
@@ -74,11 +59,9 @@ Example of particle advection around an idealised peninsula""")
                    help='Numerical method used for advection')
     args = p.parse_args()
 
-    filenames = {'U': "Agulhas/*U.nc", 'V': "Agulhas/*V.nc"}
-    variables = {'U': 'uo', 'V': 'vo'}
-    dimensions = {'lat': 'nav_lat', 'lon': 'nav_lon', 'time': 'time_counter'}
+    basename = 'Agulhas/*'
 
-    grid = Grid.from_netcdf(filenames, variables, dimensions, vmax=1e4)
+    grid = Grid.from_nemo(basename, vmax=1e4)
 
     if args.profiling:
         from cProfile import runctx
@@ -89,6 +72,5 @@ Example of particle advection around an idealised peninsula""")
                globals(), locals(), "Profile.prof")
         Stats("Profile.prof").strip_dirs().sort_stats("time").print_stats(10)
     else:
-        agulhas_example(grid, args.particles, mode=args.mode,
-                       degree=args.degree, verbose=args.verbose,
-                       output=not args.nooutput, method=method[args.method])
+        agulhas_example(grid, mode=args.mode, verbose=args.verbose,
+                        output=not args.nooutput, method=method[args.method])
